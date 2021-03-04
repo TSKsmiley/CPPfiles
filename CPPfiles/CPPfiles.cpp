7e86f95675c70a9b98286b33c2e339584cd3baf4
Arnar F <Arnarfreyr29@gmail.com>
2021-03-04 14:55:36 +0000

    Finished for todag made inial file reader and started work on "compression"

--- CPPfiles/CPPfiles.cpp
+++ CPPfiles/CPPfiles.cpp
@@ -2,11 +2,55 @@
 //
 
 #include "CPPfiles.h"
+#include "iostream"
+#include "fstream"
+#include "string"
+#include "sstream"
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+string readFile(string file) {
+	string line;
+	ostringstream result;
+	
+	ifstream myFile (file);
+
+	if (myFile.is_open()){
+		while (getline(myFile,line))
+		{
+			result << line << endl;
+		}
+		myFile.close();
+	}
+	else return "";
+	return result.str();
+}
+
+int main() {
+	string content = readFile("test.txt");
+	ostringstream newContent;
+
+	int prevCount = 1;
+
+	cout << content.length();
+
+	for (size_t i = 0; i < content.length()-1; i++)
+	{
+		if (content[i] != content[i+1] && content[i] == '/') newContent << '/';
+		else if (content[i]==content[i+1]) prevCount++;
+		else if (prevCount > 1 && content[i]!=content[i+1] ) {
+			newContent << '/' << prevCount << '/' << content[i];
+			prevCount = 1;
+		}
+		else {
+			newContent << content[i];
+		}
+	}
+	cout << newContent.str() << endl;
+	ofstream outFile;
+	outFile.open("out.txt");
+	outFile << newContent.str();
 	return 0;
 }
+
+
